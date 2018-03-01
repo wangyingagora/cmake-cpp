0035046e9b37a1be08154a1a1f5b7b46653e4f71
wangying <wangying@agora.io>
2018-03-01 14:43:01 +0800

    change to c++11

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 #include "base.hpp"
 
+struct X {
+};
+
 template<typename ...Types>
 class Tuple {
 public:
@@ -12,6 +15,7 @@ public:
 
 int main(int argc, char* argv[])
 {
+	struct X x;
 	std::cout << Tuple<int>::len << std::endl;
 	std::cout << Tuple<int, int, int>::len << std::endl;
 	return 0;
